519aff83faf1b3f92587bc30ee1a35c02175ef25
Kent Tonino <tonino.kentlouise@gmail.com>
2025-11-16 15:21:43 +0800

    feat: oh yeah

--- opencv-cpp/src/examples/0001_image.cpp
+++ opencv-cpp/src/examples/0001_image.cpp
@@ -12,20 +12,25 @@ using namespace std::filesystem;
   This example will load an image.
 */
 int ex_0001_load_image(void) {
-  string absolute_path = current_path().string();
-  string image_path = "/src/files/digital_circuit.png";
-  Mat img = imread(absolute_path + image_path, IMREAD_COLOR);
+  string directory_path = current_path().string();
+  string relative_image_path = "/src/files/digital_circuit.png";
+  string image_path = directory_path + relative_image_path;
+  Mat img = imread(image_path, IMREAD_COLOR);
 
+  // Throw error if there is a problem.
   if (img.empty()) {
     cout << "Could not read the image: " << image_path << endl;
     return 1;
   }
 
   imshow("Display window", img);
-  int k = waitKey(0); // Wait for a keystroke in the window
 
+  // Wait for a keystroke in the window.
+  int k = waitKey(0);
+
+  // Save file if s char is pressend.
   if (k == 's') {
-    imwrite("../files/digital_circuit_saved.png", img);
+    imwrite(image_path, img);
   }
 
   return 0;
