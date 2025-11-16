23db871eb7ae378cefdfd695742e4532b1eddd35
Kent Tonino <tonino.kentlouise@gmail.com>
2025-11-16 14:10:08 +0800

    chore: I do not know

--- opencv-cpp/src/examples/0001_image.cpp
+++ opencv-cpp/src/examples/0001_image.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/imgcodecs.hpp>
 
-using namespace cv;
 using namespace std;
 
 /*
   This example will load an image.
 */
-void ex_0001_load_image(void) {
-  string image_path = samples::findFile("../files/digital_circuit.png");
-  Mat img = imread(image_path, IMREAD_COLOR);
+int ex_0001_load_image(void) {
+  string image_path = cv::samples::findFile("../files/digital_circuit.png");
+  cv::Mat img = imread(image_path, cv::IMREAD_COLOR);
 
   if (img.empty()) {
-    cout << "Could not read the image." << image_path;
+    std::cout << "Could not read the image: " << image_path << std::endl;
+    return 1;
   }
+
+  imshow("Display window", img);
+  int k = cv::waitKey(0); // Wait for a keystroke in the window
+
+  if (k == 's') {
+    cv::imwrite("starry_night.png", img);
+  }
+
+  return 0;
 }
--- opencv-cpp/src/examples/examples.hpp
+++ opencv-cpp/src/examples/examples.hpp
@@ -3,6 +3,6 @@
 #include "./0001_image.cpp"
 
 // Declaratin for 0001_image functions.
-extern void ex_0001_load_image(void);
+extern int ex_0001_load_image(void);
 
 #endif
