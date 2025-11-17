7321fbd62ad27b3fc492893cb35a01a46da30874
Kent Tonino <tonino.kentlouise@gmail.com>
2025-11-17 14:02:30 +0800

    feat: show image

--- course-Udemy-Computer-Vision-Bootcamp-with-Python/src/main.py
+++ course-Udemy-Computer-Vision-Bootcamp-with-Python/src/main.py
@@ -1,3 +1,14 @@
 import cv2
+import os
+
+absolute_path = os.path.abspath("src/files/einstein.jpg")
+image = cv2.imread(absolute_path, cv2.IMREAD_GRAYSCALE)
+
+if image is not None:
+    resized_image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
+
+    cv2.imshow('Einsten', resized_image)
+    cv2.waitKey(0)
+
 
 print(cv2.__version__)
